2958101426d78a6cf8968e8dc365b4c29cec75f0
itaitzruia4 <itaitz@post.bgu.ac.il>
2023-02-11 21:24:46 +0200

    added experiments framework

--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
 DATASETS = ['adult', 'magic', 'coil2000', 'agaricus_lepiota', 'mushroom', 'ring']
+thresholds = {
+    'adult': 0.0375,
+    'magic': 0.1,
+    'coil2000': 0.1,
+    'agaricus_lepiota': 0.1,
+    'mushroom': 0.1,
+    'ring': 0.1
+}
