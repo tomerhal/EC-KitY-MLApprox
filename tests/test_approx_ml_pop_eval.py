e08e1d672cafcd25260576fb3b47e5233047a7bc
itaitzruia4 <itaitz@post.bgu.ac.il>
2023-02-13 11:48:30 +0200

    Added predict test

--- tests/test_approx_ml_pop_eval.py
+++ tests/test_approx_ml_pop_eval.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from sklearn.linear_model import SGDRegressor, LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 
 from eckity.genetic_encodings.ga.vector_individual import Vector
 from eckity.fitness.simple_fitness import SimpleFitness
@@ -45,7 +46,7 @@ class TestApproxMLPopulationEvaluator:
 			model.fit(V_train, y_train)
 
 			V_test = np.array([ind.vector for ind in X_test])
-			assert model.score(V_test, y_test) == pytest.approx(pop_eval.model.score(V_test, y_test))
+			assert model.score(V_test, y_test) == pop_eval.model.score(V_test, y_test)
 
 	def test_accumulate_population_fitness(self):
 		for model_type, params in models_params.items():
@@ -60,3 +61,25 @@ class TestApproxMLPopulationEvaluator:
 			pop_eval.fit(individuals, fitnesses)
 			for ind, fitness in zip(individuals, fitnesses):
 				assert ind.vector + [fitness] in pop_eval.df.values
+	
+	def test_predict(self):
+		for metric in [mean_absolute_error, mean_squared_error, r2_score]:
+			for model_type, params in models_params.items():
+				pop_eval = ApproxMLPopulationEvaluator(
+					model_type=model_type,
+					model_params=params,
+					scoring=metric
+				)
+				X_train = self._generate_vectors(10, (0, 9), 2)
+				y_train = [ind.get_pure_fitness() for ind in X_train]				
+				X_test = self._generate_vectors(2, (10, 19), 2)
+				y_test = [ind.fitness.fitness for ind in X_test]
+
+				pop_eval.fit(X_train, y_train)
+
+				model = model_type(**params)
+				V_train = np.array([ind.vector for ind in X_train])
+				V_test = np.array([ind.vector for ind in X_test])
+				model.fit(V_train, y_train)
+				
+				assert metric(y_test, model.predict(V_test)) == metric(y_test, pop_eval.predict(X_test))
